fbf8f3cf0c160de8041f5e8c90aff6f9290fd1c1
Jonatahn Bajada <jbajada@ryerson.ca>
2020-09-09 12:27:51 -0400

    google auth

--- fs-react/server/index.js
+++ fs-react/server/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({ hi: 'there' });
-});
+passport.use(new GoogleStrategy());
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+
+// ClientID 750061771212-5115d9v2mncmfn410p8302dje455lcvd.apps.googleusercontent.com
+
+// ClientSecret IkwBeToIZcx_yXa97XwtyhCE
